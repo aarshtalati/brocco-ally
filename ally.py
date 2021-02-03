fa5a3094be33b9efe73f09e7fb05c912cde3f9fe
Aarsh Talati <aarshstalati1989@gmail.com>
2021-02-03 17:37:07 -0500

    add boilerplate code, add loc functionality

---
+++ ally.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys
+import requests
+
+print ("Number of arguments: {0}".format(len(sys.argv)))
+print ("Arguments list: {0}".format(sys.argv))
+
---
+++ constants.py
@@ -0,0 +1,3 @@
+LOC_URL = "http://api.open-notify.org/iss-now.json"
+PASS_URL = "http://api.open-notify.org/iss-pass.json?lat={lat}&lon={lon}"
+PEOPLE_URL = "http://api.open-notify.org/astros.json"
